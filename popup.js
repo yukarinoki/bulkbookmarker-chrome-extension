c42421c72ce6eab4d36f1a676d6b5032735c3012
yukarinoki <blkheartpg@gmail.com>
2019-06-23 16:45:58 +0900

    show tab impl

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
 var tabs　= []
-var tabs_marked 
+var tabs_marked = {}
+var it = 0
 function print_tabs_of_window(window){
     tabs = window.tabs
     console.log(window)
     console.log(tabs)
     for(var tab of tabs){
-        tabs_marked += true
+        tabs_marked['id_'+tab.id] = {flag: true, content: tab};
         console.log(tab.title)
-        $("#current_tabs").append("<tr id=" + tab.id + "\" class='on'><td>" + tab.title.slice(0,28) + "</td></tr>");
+        $(".current_tabs").append('<tr id=id_' + tab.id + ' class="on"><td>' + tab.title.slice(0,28) + "</td></tr>");
+        var sel = document.querySelector("#"+"id_"+tab.id)
+        sel.eventParam = tab.id
+        console.log(sel);
+        sel.addEventListener('click', change_mark, false)
     }
+    console.log(tabs_marked);
+}
+
+function change_mark(){
+    console.log(this.id)
+    tabs_marked[this.id].flag = !tabs_marked[this.id].flag
+    if(tabs_marked[this.id].flag) this.className = 'on'
+    else this.className = 'off'
+    console.log(tabs_marked)
 }
 
 chrome.windows.getCurrent({populate: true}, print_tabs_of_window)
@@ -20,7 +34,15 @@ $(function(){
 $(function(){
     $('#reload_button').click(function(){
         console.log("reload!");
-        $('#current_tabs').empty();
+        $('.current_tabs').empty();
         chrome.windows.getCurrent({populate: true}, print_tabs_of_window)
+    });
+});
+
+$(function(){
+    $('#bookmark_button').click(function(){
+        console.log("bookmark!");
+        chrome.bookmarks.create({title: "testmake"})
+        
     });
 });
